8c7d9968555201ada65a07f6eec5164d86a82920
kush1999-h <74780178+kush1999-h@users.noreply.github.com>
2023-05-29 14:53:49 +0600

    unnecessary file delete

--- poseapi/poseapi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from poseapp.views import process_frame
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('process_frame/', process_frame),
-]
